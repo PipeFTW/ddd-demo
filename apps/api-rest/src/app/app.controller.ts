02677ffe621df667ae0e4e8078ab34e27c6c7dba
Martin Kurz <martin.kurz@bearingpoint.com>
2022-11-22 11:26:40 +0100

    [API] Add basic REST Get endpoint for users

--- apps/api-rest/src/app/app.controller.ts
+++ apps/api-rest/src/app/app.controller.ts
@@ -1,18 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import {Controller, Get, Inject} from '@nestjs/common';
 
-import { AppService } from './app.service';
+import {AppService} from './app.service';
+import {FindAllUsersUsecase, UsecaseModule} from '@ddd-demo/api/application';
+import {User} from '@ddd-demo/api/domain';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getData() {
-    return this.appService.getData();
-  }
+  constructor(private readonly appService: AppService,
+    @Inject(UsecaseModule.FIND_ALL_USERS_USECASE)
+    private readonly findAllUsersUsecase: FindAllUsersUsecase,
+  ) {}
 
   @Get('users')
-  getUsers() {
-    throw new Error('Not implemented');
+  getUsers(): Promise<User[]> {
+    return this.findAllUsersUsecase.findAllUsers();
   }
 }
--- apps/api-rest/src/app/app.module.ts
+++ apps/api-rest/src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { Module } from '@nestjs/common';
+import {Module} from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {ConfigProviderModule, UsecaseModule} from '@ddd-demo/api/application';
 
 @Module({
-  imports: [],
+  imports: [ConfigProviderModule, UsecaseModule.register()],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
