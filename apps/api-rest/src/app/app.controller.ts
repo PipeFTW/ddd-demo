c1e320914e9b069c7778262b5e8ee8c2c14f8766
Martin Kurz <martin.kurz@bearingpoint.com>
2022-11-22 14:04:20 +0100

    [API-REST] Add a second usecase for showcase

--- apps/api-rest/src/app/app.controller.ts
+++ apps/api-rest/src/app/app.controller.ts
@@ -1,18 +1,27 @@
-import {Controller, Get, Inject} from '@nestjs/common';
-
+import {Controller, Get, Inject, Param} from '@nestjs/common';
 import {AppService} from './app.service';
-import {FindAllUsersUsecase, User} from '@ddd-demo/api/domain';
+import {FindAllUsersUsecase, FindUserByIdUsecase, User} from '@ddd-demo/api/domain';
 import {UserUsecase} from '@ddd-demo/api/application';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService,
+  constructor(
+    private readonly appService: AppService,
     @Inject(UserUsecase.FIND_ALL_USERS_USECASE)
     private readonly findAllUsersUsecase: FindAllUsersUsecase,
-  ) {}
+    @Inject(UserUsecase.FIND_USER_BY_ID_USECASE)
+    private readonly findUserByIdUsecase: FindUserByIdUsecase,
+  ) {
+  }
 
   @Get('users')
   getUsers(): Promise<User[]> {
     return this.findAllUsersUsecase.findAllUsers();
   }
+
+  @Get('users/:id')
+  getUserById(@Param('id') id: number): Promise<User> {
+    return this.findUserByIdUsecase.findUserById(id);
+  }
+
 }
